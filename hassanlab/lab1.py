c6bf7fce9fdf6694935bd5baaf5aca7cfadde81c
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-08-26 23:06:29 -0700

    This is my frist file  lecture parts lab python  thank you

---
+++ hassanlab/lab1.py
@@ -0,0 +1,50 @@
+markslist = [40,44,39,43,44,66,77,88,]
+print(type(markslist))
+print(type(markslist[3]))
+markslist[0] = 10
+print(markslist[0])
+print(markslist)
+
+
+
+#creating list of different data tyoe
+print("showing list whit value of different data types")
+mangolist = ["hassan", 40 , "bdsm-s20-001" ]
+print(mangolist)
+print(type(mangolist))
+print(mangolist[0])
+print(type(mangolist[0]))
+print(mangolist[1])
+print(type(mangolist[1]))
+print(mangolist[2])
+print(type(mangolist[2]))
+
+#creating a dictionary datatype in python
+studentdict_1 = {"name": ,"hassan": ,"age":36 ,"marks":100}
+print(student_1)
+print("name of student:")
+print(studentdict_1["name"])
+print("age of student:")
+print(studentdict_1["age"])
+print("marks of student:")
+print(studentdict_1["marks"])
+studentdict_1 = {"name": "hassan": "age":"rollno":"bdsm-s20-001"}
+studentdict_2 = {"name": "ali": "age":"rollno":"bdsm-s20-002"}
+studentdict_3 = {"name": "qasim": "age":"rollno":"bdsm-s20-003"}
+studentdict_4 = {"name": "raza": "age":"rollno":"bdsm-s20-004"}
+studentdict_5 = {"name": "ahmad": "age":"rollno":"bdsm-s20-005"}
+liststudent_record = [stddict_1,stddict_2,stddict_3,stddict_4,stddict_5]
+print(liststudent_record)
+print(liststudent_record[0])
+print(liststudent_record[0]["name"])
+print(liststudent_record[1]["ali"])
+print(liststudent_record[2]["qasim"])
+print(liststudent_record[3]["raza"])
+print(liststudent_record[4]["ahmad"])
+
+
+
+
+
+
+

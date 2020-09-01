b542ca4d2793919d5f187f5a68fa721039eb7933
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-08-31 21:34:12 -0700

    my 2 lecture is condition if else esif

---
+++ OOPFALLS20.py
@@ -0,0 +1,14 @@
+# range in python
+x = rangs("2 , 10")
+print(x)
+
+# tuples in python
+
+mytuple = (30, 20, 56, 78, )
+print("mytuple")
+
+# boolean in python
+myvariable1 = ture
+print(type(myvariable1))
+
+#
---
+++ conditionlec2.py
@@ -0,0 +1,44 @@
+#we learn simple if condition
+mobileprice = 5000
+if mobileprince < 10000:
+    print("purchase mobile phone")
+    print("purchase only one")
+ # ---------------------------------------------------------------------------
+print("phone is good ")
+
+# if esle condition
+studentmarks  = 40
+if studentmarks >=40:
+    print("student is pass")
+    print("student is eligible for degree")
+
+print ("he/she was student of superior university")
+# ------------------------------------------------------------------------------
+
+
+
+mobileprice = 10000
+if mobileprice < 20000:
+    print("purchase mobile phone ")
+    print ("purchase nokia phone ")
+elif mobileprice<2500:
+    print("purchase 2 mobile phones ")
+else:
+    print("don't purchase this phone")
+    print("i don't like phone")
+#        ----------------------------------------------------------------------------------
+
+#nested if condition
+
+num1 = 10
+num2 = 20
+num3 = 40
+if num1 > num2:
+    print("number 1 is greater than number 2")
+    if num1 > num3:
+        print("number 1 is greater than all number ")
+
+else:
+    print ("number 2 is greater than number 1 ")
+# ------------------------------------------------------------------------
+

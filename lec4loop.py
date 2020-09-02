5384f2bef28e7bad890597c200c09e0529c7d12f
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-09-01 22:58:19 -0700

    my lecture 3 loops for loop while loop

---
+++ lec4loop.py
@@ -0,0 +1,32 @@
+# loop in python
+# loop are the twp types of loops
+# conter loop--also known as for loop
+# non counter loop also known as while loop
+# nested loop---also known as inside loop
+# while lop
+# print first ten natural number
+counter = 1
+while counter <=10:
+    print (counter)
+    counter = counter +1
+print ("allah is grate")
+print("------------------------------------------------")
+x = range(1,10)
+for number in x:
+    ans = number +1
+    print(ans)
+print("____________________________________________")
+stdname = ["hassan","sir qasim"]
+for eachstudent in stdname :
+    print(eachstudent + "is my student")
+print("+++++++++++++++++++++++++++++++++++++++++++++++")
+i =1
+while i <=10:
+    print("_________________")
+    print (i)
+    j = 15
+    while j = <=10:
+        print(j)
+        j = j + 1
+        print("__________")
+    i = i +i
---
+++ ooplec4loops.py
@@ -0,0 +1,14 @@
+# taking value form user at run time
+studentname = input ("enter your name")
+studentname = input ("enter your age")
+markseng = int(input("enter marks englisg"))
+marksurdu = int(input("enter makrs urdu "))
+totalmarks = markseng + markseng
+
+print("-------------------------------")
+print("my name is ")
+print(type(studemtname))
+print("----------------")
+print("my age is")
+print(type(studentage))
+print("--------------")

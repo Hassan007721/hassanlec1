33280267c9a826b2d78390f7de082c56b6753767
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-09-02 22:39:35 -0700

    task lab while loop for loop even and odd

---
+++ task3checkevenandodd.py
@@ -0,0 +1,32 @@
+#taking number from user and checking if it is even and odd
+takingvalue = int(input("enter the value :"))
+modevalue = takingvalue % 2
+if modevalue == 0 :
+    print(takingvalue,"is an even value ")
+else:
+    print (takingvalue,"is a odd value ")
+ print("-------------------------------------------------------------")
+    # taking the rang of the number from the user find all the even and odd number present in that range
+lowerValue =int(input("enter lowerValue"))
+ upperValue= int(input("enter uperValue"))
+for eachnumber in range(lowervalue,uppervalue+1)
+    modevalue = eachnumber % 2
+    if modevalue == 0:
+        print(eachnumber, "is a even")
+    else:
+        print(eachnumber,"is an odd")
+print("----------------------------------------------------------------------")
+    # taking the range number form the user to find all the even and odd number present is taht range
+lowervalue =int(input("enter lowerValue"))
+ uppervalue= int(input("enter uperValue"))
+i = upperlimt
+while (i <= upervalue):
+     modevalue = eachnumber % 2
+     if modevalue == 0:
+         print(i, "is an even")
+else:
+    print( i,"is an odd")
+    i=i+1
+
+
+

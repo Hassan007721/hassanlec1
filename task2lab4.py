4746b79bc23d413389e145687f9ffe89c7632448
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-09-16 23:34:15 -0700

    This is task 2 -list function class

---
+++ task2lab4.py
@@ -0,0 +1,102 @@
+class uni()
+    uniName = ""
+    uniCity = ""
+    uniAge = ""
+
+    def introduction(self):
+        print("my uni name: " + self.name)
+        print("my uni city: " + self.city)
+        print("my uni age: " + str(self.uniAge) + "year")
+
+
+
+# create object of class uni
+uni1 = university()
+uni1.uniname = "superior university gold campus"
+uni1.uniage = 1998
+uni1.unicity = "lahore"
+uni1.introduction()
+
+uni2 = university()
+uni2.uniname = "superior university gold campus"
+uni2.uniage = 1998
+uni2.unicity = "lahore"
+uni2.introduction()
+
+uni3 = university()
+uni3.uniname = "superior university gold campus"
+uni3.uniage = 1998
+uni3.unicity = "lahore"
+uni3.introduction()
+
+uni3 = university()
+uni3.uniname = "superior university gold campus"
+uni3.uniage = 1998
+uni3.unicity = "lahore"
+uni3.introduction()
+
+uni4 = university()
+uni4.uniname = "superior university gold campus"
+uni4.uniage = 1998
+uni4.unicity = "lahore"
+uni4.introduction()
+
+uni5 = university()
+uni5.uniname = "superior university gold campus"
+uni5.uniage = 1998
+uni5.unicity = "lahore"
+uni5.introduction()
+universitylist = [std1, std2  std3, std4, std5]
+for eachUni in uniList:
+    eachUni.unversity_information()
+
+print("_______________________________________________")
+
+
+# Teacher
+# teacher class will have three variable
+
+
+class tch()
+    tchName = ""
+    tchSubject = ""
+    tchAge = ""
+
+    def introduction(self):
+        print("my tch name: " + self.name)
+        print("my tch subject: " + self.subject)
+        print("my uni age: " + str(self.tchAge) + "year")
+
+    # create object of class teacher
+    tch = teacher()
+
+tch1.tchName = "sir qasim"
+tch1.tchSubject = "oop"
+tch1.tchAge = "25"
+tch1.introduction()
+
+tch2 = teacher()
+tch2.tchName = "sir ahmad"
+tch2.tchSubject = "introduction to computer"
+tch2.tchAge = "25"
+tch2.introduction()
+
+tch = teacher()
+tch3.tchName = "sir sagheer"
+tch3.tchSubject = "maths"
+tch3.tchAge = "35
+tch3.introduction()
+
+tch = teacher()
+tch4.tchName = "sir qasim"
+tch4.tchSubject = "database"
+tch4.tchAge = "25"
+tch4.introduction()
+
+tch = teacher()
+tch5.tchName = "maam nosheen"
+tch5.tchSubject = "tech and business"
+tch5.tchAge = "26"
+tch5.introduction()
+for eachTch in tchList:
+    each.teacher_information()
---
+++ tasklab4.py
@@ -0,0 +1,37 @@
+# task-1 create two classes university and teacher
+# unversity class will have three variable
+class uni ()
+    uniName = ""
+    uniCity = ""
+    uniAge = ""
+    def introduction(self):
+        print ("my uni name: " + self.name)
+        print ("my uni city: " + self.city)
+        print ("my uni age: " + str(self.uniAge) + "year")
+# create object of class uni
+uni = university()
+uni.uniname = "superior university gold campus"
+uni.uniage = 1998
+uni.unicity = "lahore"
+uni.introduction()
+print("_______________________________________________")
+    
+    # Teacher
+    # teacher class will have three variable
+
+
+class tch ()
+    tchName = ""
+    tchSubject = ""
+    tchAge = ""
+    def introduction(self):
+        print ("my tch name: " + self.name)
+        print ("my tch subject: " + self.subject)
+        print ("my uni age: " + str(self.tchAge) + "year")
+# create object of class teacher
+    tch = teacher()
+   tch.tchName = "superior university gold campus"
+   tch.tchSubject = "oop"
+    tch.tchAge = "25"
+    tch.introduction()
+

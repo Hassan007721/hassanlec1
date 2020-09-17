ae961c85a4a0560abb7cc67aba7578e0be11091d
MEHAR HASSAN <meharhassan5862126@gmail.com>
2020-09-16 22:02:47 -0700

    This is lecture 7 -class function

---
+++ lecture7function.py
@@ -0,0 +1,18 @@
+# class Function
+class human:
+    name = ""
+    age = ""
+    def introduction(self):
+        print("My name is: " + self.name)
+        print("My age is: " + str(self.age) + "year")
+h1 = human()
+h1.name = "sir qasim"
+h1.age = 25
+h1.introduction()
+print("______________________________________________")
+h2 = human()
+h2.name = "hassan"
+h2.age = 21
+h2.introduction()
+print("______________________________________________")
+
